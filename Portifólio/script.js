5c454f19b0f2736c521ff053d04012b68c1df4ad
RobertoConrado <robertoconradorc@gmail.com>
2022-05-10 13:49:50 -0300

    Add JS

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -1,3 +1,27 @@
-function onclick() {
-    alert('Seja bem vindo')
-}
+
+const dataAtual = new Date();
+
+const dia = String(dataAtual.getDate()).padStart(2, '0')
+const mes = String(dataAtual.getMonth() + 1).padStart(2, '0')
+const ano = dataAtual.getFullYear()
+
+const data = `${dia}/${mes}/${ano}`
+const time = document.getElementById('time')
+
+time.innerHTML = `<time>Ao dia ${data}</time>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
